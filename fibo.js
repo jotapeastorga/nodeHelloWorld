c6394453333406d683be86bf23ea865c49d10c14
jotape <jotapeastorga@gmail.com>
2019-05-16 12:16:40 -0400

    modificacion de variable num para casos donde 1 o 2 sea la entrada

--- fibo.js
+++ fibo.js
@@ -1,11 +1,11 @@
-console.log( "valor"  + fibo(7));
+console.log( "valor "  + fibo(7));
 
 
 
 function fibo(num){
 
     if (num === 1 || num === 2){
-        return 1;
+        return num;
      }else{
         return fibo(num-1)+fibo(num-2)   
     }
